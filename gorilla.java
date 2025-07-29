8b4c7bada5851b0d450870b57aee6c74590581d7
happypattys <etzzsaguid@gmail.com>
2025-07-29 11:17:46 +0800

    gorilla

--- gorilla.java
+++ gorilla.java
@@ -1,1 +1,27 @@
+interface Animal {
+    boolean feed(boolean timeToEat);
+    void groom();
+    void pet();
+}
 
+public class Gorilla implements Animal {
+    public boolean feed(boolean timeToEat) {
+        System.out.println("put gorilla food into cage");
+        return timeToEat;
+    }
+
+    public void groom() {
+        System.out.println("lather, rinse, repeat");
+    }
+
+    public void pet() {
+        System.out.println("pet at your own risk");
+    }
+
+    public static void main(String[] args) {
+        Gorilla g = new Gorilla();
+        g.feed(true);
+        g.groom();
+        g.pet();
+    }
+}
